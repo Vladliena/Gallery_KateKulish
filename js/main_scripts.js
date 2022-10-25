982b9fba7f86cf711c5ec30a986691001e98d750
vladliena <sotnikovavladlena@gmail.com>
2022-10-25 12:25:35 +0300

    add slick carousel

---
+++ js/main_scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.gallery-banner').slick({
+        setting-name: setting-value
+});
+});
+
+$('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+});
